1ffc45ec2affb1b055e7159fdef8f49a7653e620
peter <withtvpeter@gmail.com>
2018-10-01 12:48:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+function getFirstSelector(selector)
+{
+   return document.querySelector('selector');
+}
 
+function nestedTarget(target)
+{
+  var el = document.getElementById('#nested');
+  var tg = el.querySelector('.target');
+  return tg;
+}
+
+function increaseRankBy(n)
+{
+  var listLi = document.querySelectorAll(".ranked-list");
+   
+  for( let i=1; i<=n; i++)
+  {
+     var li = document.createElement("li");
+     var text = document.createTextNode("Hello this is the new text");
+     var cm = li.appendChild(text);
+     listLi.appendChild(cm);
+    
+  }
+}
+
+function deepestChild()
+{
+  var nd = document.querySelectorAll('div#grand-node div div div div');
+  
+  return nd;
+  
+}
